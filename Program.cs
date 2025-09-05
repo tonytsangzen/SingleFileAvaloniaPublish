e18eb8da530d8173ebcd32c074af7a0a360277aa
tony <tonytsangzen@gmail.com>
2025-09-06 00:26:18 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -10,25 +11,45 @@ namespace ava;
 
 class Program
 {
-    static readonly List<string> extraLibs = [];
-
+   public static void Restart()
+    {
+        string? currentExe = Environment.ProcessPath;
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = currentExe,
+            Arguments = Environment.CommandLine.Replace($"\"{currentExe}\"", "").Trim(),
+            UseShellExecute = true
+        };
+        Process.Start(startInfo);
+        Environment.Exit(0);
+    }
+    
     [ModuleInitializer]
     internal static void Initialize()
     {
         var assembly = Assembly.GetExecutingAssembly();
+        string tempPath = Path.Combine(Path.GetTempPath(), assembly.GetName().Name!);
+        Directory.CreateDirectory(tempPath);
+        Console.WriteLine($"tempPath: {tempPath}");
         var libs = assembly.GetManifestResourceNames();
         foreach (var lib in libs){
             using var stream = assembly.GetManifestResourceStream(lib);
             if (stream != null)
             {
-                var extra = string.Join('.', lib.Split('.').Skip(3));
-                if(extra.Length > 0 && !File.Exists(extra)){
+                var fn = string.Join('.', lib.Split('.').Skip(3));
+                var extra = Path.Combine(tempPath, fn);
+                if(fn.Length > 0 && !File.Exists(extra)){
                     using var file = File.Create(extra);
                     stream!.CopyTo(file);
                     file.Close();
-                    extraLibs.Add(extra);
                 }
             }
+        }
+
+        if(Environment.GetEnvironmentVariable("DYLD_LIBRARY_PATH") != tempPath)
+        {
+            Environment.SetEnvironmentVariable("DYLD_LIBRARY_PATH", tempPath);
+            Restart();
         }
     }
 
@@ -44,10 +65,9 @@ class Program
                 .StartWithClassicDesktopLifetime(args);
         }finally
         {
-            foreach (var lib in extraLibs)
-            {
-                File.Delete(lib);
-            }
+            var assembly = Assembly.GetEntryAssembly();
+            string tempPath = Path.Combine(Path.GetTempPath(), assembly.GetName().Name!);
+            Directory.Delete(tempPath, true);
         }
     }
 }
